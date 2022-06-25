659fdc666b5635317add48115812b4f7ef34ebb5
Miler Kamil <mmiler6969@gmail.com>
2022-06-25 16:16:51 +0200

    Uzupełnienie ciała typu hammingDistancei dodanie voida createFile1

--- main.cpp
+++ main.cpp
@@ -50,3 +50,25 @@ int main(int argc, char * argv[])
 {
         return 0;
 }
+uint8_t hammingDistance(uint8_t n1, uint8_t n2)
+{
+    uint8_t x = n1 ^ n2;
+    uint8_t setBits = 0;
+    while (x > 0)
+    {
+        setBits += x & 1;
+        x >>= 1;
+    }
+    return setBits;
+}
+
+void createFile1(const std::string name, const int count, const char value)
+{
+    std::fstream f;
+    f.open(name.c_str(), std::ios::binary | std::ios::out);
+    for (int i = 0; i < count; i++)
+    {
+        f.write((char*)&value,1);
+    }
+    f.close();
+}
